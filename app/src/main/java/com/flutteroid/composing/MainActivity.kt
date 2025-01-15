2b1c8eaf25e046780ab16bcb6a05bededa52cee6
Siddharth Singh <siddharth.singh@kobil.com>
2025-01-15 11:24:55 +0530

    12th Jan

--- app/src/main/java/com/flutteroid/composing/MainActivity.kt
+++ app/src/main/java/com/flutteroid/composing/MainActivity.kt
@@ -5,8 +5,15 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,11 +27,15 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -32,15 +43,115 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
-      ShowMyInputField()
+
+      //LoginForm()
+      //ShowMyImageFromNetwork()
     }
   }
+
+
+}
+
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun ShowMyStack() {
+  Text(text = "Hello WOrld")
+  Text(text = "Hello WOrld sjkd")
+  Text(text = "Hello WOrld dsfsdfsd")
+  Text(text = "Hello WOrld dsfsdfsdfsdf")
+  Text(text = "Hello WOrld sdfsdfsdfsdfsdfsd")
+  Text(text = "Hello WOrld dsfsdfsdfsdfsdfsdfsdf")
+}
+
+
+@Composable
+fun ShowMyImageFromNetwork() {
+  AsyncImage(
+    model = "https://reqres.in/img/faces/8-image.jpg",
+    contentDescription = "network_image"
+  )
+}
+
+
+@Composable
+fun ShowMyImage() {
+  Box(
+    modifier = Modifier.background(Color.Black)
+  ) {
+    Image(
+      painter = painterResource(id = R.drawable.image),
+      contentDescription = "image",
+      contentScale = ContentScale.Crop,
+      modifier = Modifier
+        .height(200.dp)
+        .fillMaxSize()
+    )
+  }
+
+}
+
+
+@Composable
+fun LoginForm() {
+  Column(
+    modifier = Modifier
+      .fillMaxSize()
+      .background(color = Color(0xFF042F68))
+      .padding(horizontal = 20.dp),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Column(
+      modifier = Modifier.weight(1f),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Text(
+        text = "facebook", style = TextStyle(
+          color = Color.White,
+          fontSize = 28.sp,
+          fontWeight = FontWeight.ExtraBold
+        )
+      )
+      Spacer(modifier = Modifier.height(30.dp))
+      TextField(value = "", onValueChange = {
+
+      }, placeholder = {
+        Text(text = "email or phone")
+      }, modifier = Modifier.fillMaxWidth())
+      Spacer(modifier = Modifier.height(10.dp))
+      TextField(value = "", onValueChange = {
+
+      }, placeholder = {
+        Text(text = "password")
+      }, modifier = Modifier.fillMaxWidth())
+      Spacer(modifier = Modifier.height(20.dp))
+      Button(
+        onClick = {},
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(5.dp),
+        colors = ButtonDefaults.buttonColors().copy(containerColor = Color(0xFF031936))
+      ) {
+        Text(text = "Log In")
+      }
+    }
+    Text(
+      text = "sign up for facebook",
+      style = TextStyle(color = Color.White),
+      fontSize = 16.sp,
+      modifier = Modifier.padding(bottom = 30.dp)
+    )
+  }
+
 }
 
 
@@ -56,7 +167,6 @@ fun ShowMyText() {
 }
 
 @Composable
-@Preview(showBackground = true, showSystemUi = true)
 fun ShowMyInputField() {
   val data = remember { mutableStateOf("") }
   val isPasswordShown = remember {
